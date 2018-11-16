f344a7a3f229923cbc65e546e2a46befab81a48d
teiserse <teiserse@tcd.ie>
2018-11-16 10:41:47 +0000

    prepared the DETAILS file to be used

--- ghdata/src/main.rs
+++ ghdata/src/main.rs
@@ -1,12 +1,18 @@
 extern crate github_rs;
 extern crate serde_json;
 extern crate mysql;
+extern crate regex;
 use github_rs::client::{Executor, Github};
 use serde_json::Value;
 use serde_json::to_string_pretty;
-use mysql;
+use std::fs;
+use regex::Regex;
 
 fn main() {
+    let file = fs::read_to_string("../DETAILS").unwrap();
+    let re_find = Regex::new(r"token:[ \t]*([0-9a-f]+)[\n]").unwrap();
+    let token = &re_find.captures(&file).unwrap()[1];
+    println!("{}", token);
     let client = Github::new("af374f41456a915d346f97cfdd42cf1d30df0b58").unwrap();
     let me = client.get()
         .repos()
@@ -24,7 +30,6 @@ fn main() {
         },
         Err(e) => println!("{}", e)
     }
+    //let pool = mysql::Pool::new("mysql://et-vis:et-vis@localhost:3307/et-vis").unwrap();
 
-    let pool = mysql::Pool::new("mysql://et-vis:et-vis@localhost:3307/et-vis").unwrap();
-    
 }
