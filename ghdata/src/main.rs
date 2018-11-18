67bfb5ae7bc5197d3b11f428aaab6215e5c64f22
teiserse <teiserse@tcd.ie>
2018-11-18 12:56:19 +0000

    formed list of top 30 contributors as a Vec<String>

--- ghdata/src/main.rs
+++ ghdata/src/main.rs
@@ -4,33 +4,43 @@ extern crate mysql;
 extern crate regex;
 use github_rs::client::{Executor, Github};
 use serde_json::Value;
-use serde_json::to_string_pretty;
+use serde_json::to_string;
 use std::fs;
 use regex::Regex;
 
 fn main() {
     let file = fs::read_to_string("../DETAILS").unwrap();
-    /*
+
     let re_find_token = Regex::new(r"token:[ \t]*([0-9a-f]+)[\n]").unwrap();
     let token = &re_find_token.captures(&file).unwrap()[1];
+
     let client = Github::new(token).unwrap();
-    let me = client.get()
+    let initial = client.get()
         .repos()
         .owner("rust-lang")
         .repo("rust")
         .contributors()
         .execute::<Value>();
-    match me {
-        Ok((headers, status, json)) => {
-            println!("{}", headers);
-            println!("{}", status);
+    let mut names :Vec<String> = Vec::new();
+    match initial {
+        Ok((_headers, _status, json)) => {
+            //println!("{}", headers);
+            //println!("{}", status);
             if let Some(json) = json{
-                println!("{}", to_string_pretty(&json).unwrap());
+                //println!("{}", to_string_pretty(&json).unwrap());
+                //let result = to_string(&json).unwrap();
+                for contrib in 0..30 {
+                    names.push(to_string(&json[contrib]["login"]).unwrap().replace("\"",""));
+                }
+                for name in names {
+                    println!("{}",name);
+                }
             }
         },
         Err(e) => println!("{}", e)
     }
-    */
+
+    /*
     let re_find_msuser = Regex::new(r"msuser:[ \t]*(?P<user>.+)[\n]").unwrap();
     let re_find_mspass = Regex::new(r"mspass:[ \t]*(?P<pass>.+)[\n]").unwrap();
     let msuser = &re_find_msuser.captures(&file).unwrap()[1];
@@ -48,5 +58,5 @@ fn main() {
               println!("{}",val);
           }
         }).unwrap();
-
+    */
 }
