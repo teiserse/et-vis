fcfbbd1da8d553ac02a70c88b2fd0b0b95ee8c5c
teiserse <teiserse@tcd.ie>
2018-11-18 11:53:09 +0000

    added initial code for connecting to MySQL

--- ghdata/src/main.rs
+++ ghdata/src/main.rs
@@ -10,8 +10,9 @@ use regex::Regex;
 
 fn main() {
     let file = fs::read_to_string("../DETAILS").unwrap();
-    let re_find = Regex::new(r"token:[ \t]*([0-9a-f]+)[\n]").unwrap();
-    let token = &re_find.captures(&file).unwrap()[1];
+    /*
+    let re_find_token = Regex::new(r"token:[ \t]*([0-9a-f]+)[\n]").unwrap();
+    let token = &re_find_token.captures(&file).unwrap()[1];
     let client = Github::new(token).unwrap();
     let me = client.get()
         .repos()
@@ -29,6 +30,23 @@ fn main() {
         },
         Err(e) => println!("{}", e)
     }
-    //let pool = mysql::Pool::new("mysql://et-vis:et-vis@localhost:3307/et-vis").unwrap();
+    */
+    let re_find_msuser = Regex::new(r"msuser:[ \t]*(?P<user>.+)[\n]").unwrap();
+    let re_find_mspass = Regex::new(r"mspass:[ \t]*(?P<pass>.+)[\n]").unwrap();
+    let msuser = &re_find_msuser.captures(&file).unwrap()[1];
+    let mspass = &re_find_mspass.captures(&file).unwrap()[1];
+    let mut connection = String::from("mysql://");
+    connection.push_str(msuser);
+    connection.push_str(":");
+    connection.push_str(mspass);
+    connection.push_str("@localhost:3306/et-vis");
+    let pool = mysql::Pool::new(&connection).unwrap();
+    pool.prep_exec("SHOW TABLES",()).map(
+        |result|{
+          for row in result {
+              let val : String = mysql::from_row(row.unwrap());
+              println!("{}",val);
+          }
+        }).unwrap();
 
 }
